f57650668287f67fd3ed996ea287e0f7a63a6fad
hectorSampieri <hoscmx@outlook.com>
2022-04-19 09:45:31 -0500

    2.Diseño de Software con TDD

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,21 @@
+const Spiderman = require('../app/spiderman')
+describe("Unit test for Spiderman class", () => {
+    test('1) Create an Spiderman Object', () => {
+      const andrewGarfield= new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+
+      expect(andrewGarfield.name).toBe("Spiderman Sony")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.movies).toBe(2)
+      expect(andrewGarfield.studio).toBe("Sony")
+    });
+  })
+
+  describe("Unit test for Spiderman class", () => {
+    test('2) User the method getinfo()', () => {
+      const tomHolland= new Spiderman("Spiderman Marvel",25,"Tom Holland",5,"Marvel")
+      
+      expect(tomHolland.getinfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+     
+    });
+  })
